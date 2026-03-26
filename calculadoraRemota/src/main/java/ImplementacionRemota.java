eb18b2f5fd5fe6c606316c8d2ff7acd185aa39e0
Marcelo9987 <imarcelo8000@gmail.com>
2026-03-26 13:20:22 +0100

    Tests implementados y probados

--- calculadoraRemota/src/main/java/ImplementacionRemota.java
+++ calculadoraRemota/src/main/java/ImplementacionRemota.java
@@ -19,11 +19,19 @@ public class ImplementacionRemota implements IComun {
 
     @Override
     public float dividir(float numero1, float numero2) throws RemoteException {
+        if(numero2 == 0)
+        {
+            throw new RemoteException("No se puede dividir entre cero");
+        }
         return numero1 / numero2;
     }
 
     @Override
     public float raiz(float numero) throws RemoteException {
+        if (numero < 0)
+        {
+            throw new RemoteException("No se puede calcular la raíz de un número negativo");
+        }
         return (float) Math.sqrt(numero);
     }
 
